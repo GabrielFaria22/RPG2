71cbbff0dec244c739d284afcb35aec3d532e5dc
Gabriel Faria <42048534+GabrielFaria22@users.noreply.github.com>
2019-04-02 09:31:18 -0300

    Cadastro de personagem pronto

--- play/src/play/App.java
+++ play/src/play/App.java
@@ -1,59 +1,22 @@
 package play;
+
 import java.util.Scanner;
 import play.Char;
 
-
-
 public class App {
-	public static void main(String[]Args) {
+	public static void main(String[] Args) {
+
+		// carregando mapa
+		Mapa map = new Mapa();
+
+		Scanner leitor = new Scanner(System.in);
 		
-		
-		//carregando mapa
-		Mapa map=new Mapa();
-		
-		
-		Scanner leitor=new Scanner(System.in);
-		int ptsatributos=10,repet=0;
-		Char novop=new Char();
-		String nome=new String();
-		int forca = 0,destreza=0,inteligencia=0;
-		System.out.println("crie seu personagem");
-		
-		
-		System.out.println("nome: ");
-		nome=leitor.nextLine();
-		
-			while(ptsatributos>=2) {
-				switch(repet) {
-				case 0:
-					System.out.println("voce tem "+ptsatributos+" pontos de atributos restantes");
-					System.out.println("forca: ");
-					forca=leitor.nextInt();
-					ptsatributos=ptsatributos-forca;
-					repet++;
-					break;
-				case 1:
-					System.out.println("voce tem "+ptsatributos+" pontos de atributos restantes");
-					System.out.println("destreza: ");
-					destreza=leitor.nextInt();
-					ptsatributos=ptsatributos-destreza;
-					repet++;
-					break;
-				case 2:
-					System.out.println("voce tem "+ptsatributos+" pontos de atributos restantes");
-					System.out.println("inteligencia: ");
-					inteligencia=leitor.nextInt();
-					ptsatributos=ptsatributos-inteligencia;
-					repet++;
-					break;
-				}
-			}
-			System.out.println("acabaram os pontos");
-		
-		Char protagonista=new Char(nome, forca, destreza, inteligencia);
-		
-		protagonista.fichadopersonagem();
+		Char principal=new Char();
+		principal.criarpersonagem();
+		principal.fichadopersonagem();
 	}
 	
-
 }
+		
+
+	
--- play/src/play/Char.java
+++ play/src/play/Char.java
@@ -1,5 +1,5 @@
 package play;
-
+import play.Item;
 import java.util.Scanner;
 import play.Item;
 
@@ -7,11 +7,8 @@ public class Char {
 	Scanner leitor=new Scanner(System.in);
 	public String nome=new String();
 	public int forca,destreza,inteligencia,hp,valorataque;
-	public Item[] mochila=new Item[10];
 	
-	if(this.hp<=0) {
-		System.out.println(this.nome+ "morreu!");
-	}
+	
 	
 	public Char(){
 		this.nome=nome;
@@ -30,9 +27,52 @@ public class Char {
 		
 	}
 	
+	public void criarpersonagem() {
+		
+		int ptsatributos = 10, repet = 0;
+		System.out.println("crie seu personagem");
+
+		System.out.println("nome: ");
+		this.nome = leitor.nextLine();
+
+		while (ptsatributos >= 2) {
+			switch (repet) {
+			case 0:
+				System.out.println("voce tem " + ptsatributos + " pontos de atributos restantes");
+				System.out.println("forca: ");
+				this.forca = leitor.nextInt();
+				ptsatributos = ptsatributos - this.forca;
+				repet++;
+				break;
+			case 1:
+				System.out.println("voce tem " + ptsatributos + " pontos de atributos restantes");
+				System.out.println("destreza: ");
+				this.destreza = leitor.nextInt();
+				ptsatributos = ptsatributos - this.destreza;
+				repet++;
+				break;
+			case 2:
+				System.out.println("voce tem " + ptsatributos + " pontos de atributos restantes");
+				System.out.println("inteligencia: ");
+				this.inteligencia = leitor.nextInt();
+				ptsatributos = ptsatributos - this.inteligencia;
+				repet++;
+				break;
+			}
+		}
+		System.out.println("acabaram os pontos.");
+
+		
+		
+		
+		
+	}
+		
+	}
+	
 	
 
-}
+
 
 
 
